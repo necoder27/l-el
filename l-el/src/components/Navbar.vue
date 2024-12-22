532143fdcd92d7c6d2922c5881104cabf28e75e0
necoder27 <laetitiaelbadry@gmail.com>
2024-12-22 02:22:26 +0100

    minor: changed navbar again
    unsure which indicator of selection to choose

--- l-el/src/components/Navbar.vue
+++ l-el/src/components/Navbar.vue
@@ -1,19 +1,24 @@
 <template>
-  <div class="flex flex-row h-10 w-screen select-none top-0 py-12 text-2xl fixed bg-[#1e1e1e]/95 backdrop-blur-md border-b border-b-gray-500">
-    <div class="w-2/3 flex flex-row items-center px-12">
-      Laetitia El Badry
+  <div>
+    <div class="flex flex-row h-10 w-screen select-none top-0 py-12 text-2xl fixed bg-[#1e1e1e]/75 backdrop-blur-md">
+      <div class="w-2/3 flex flex-row items-center px-12">
+        Laetitia El Badry
+      </div>
+      <div class="flex flex-row items-center justify-between w-2/4 px-12">
+        <NavbarLink to="/" text="home" />
+        <NavbarLink to="/projects" text="projects" />
+        <NavbarLink to="/" text="about" />
+        <NavbarLink to="/cat" text="cat" />
+      </div>
     </div>
-    <div class="flex flex-row items-center justify-between w-1/3 px-12">
-      <NavbarLink to="/" text="home" />
-      <NavbarLink to="/projects" text="projects" />
-      <NavbarLink to="/" text="about" />
-      <NavbarLink to="/cat" text="cat" />
-    </div>
+    <span class="block bg-white h-[0.05125vh] mt-24 w-full fixed"></span>
   </div>
 </template>
 
 <script setup lang="ts">
   import NavbarLink from "@/components/NavbarLink.vue";
+
+  //mx-[2.5vw] w-[95vw]
 </script>
 
 <style scoped>
--- l-el/src/components/NavbarLink.vue
+++ l-el/src/components/NavbarLink.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="group" @click="changePage(text)">
-    <RouterLink :to="`/${text}`">{{ text }}</RouterLink>
-    <span v-if="selectedPage != text" class="block max-w-0 group-hover:max-w-full h-0.5 bg-white transition-all ease-in-out duration-500"></span>
-    <span v-else class="block max-w-full h-0.5 bg-white"></span>
+    <RouterLink :to="`/${text}`" class="flex flex-row group">
+      <span :class="[selectedPage === text ? 'opacity-100' : 'opacity-0',  'group-hover:opacity-100', 'transition-all duration-300 ease-in-out']">
+        >&nbsp; <!-- //&nbsp; -->
+      </span>
+      {{ text }}
+    </RouterLink>
+    <span v-if="selectedPage != text" class="block max-w-0 h-0.5 bg-white transition-all ease-in-out duration-300"></span>
+    <!--<span v-else class="block max-w-full h-0.5 bg-white"></span>     + add group-hover:max-w-full above -->
   </div>
 </template>
 
