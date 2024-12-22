5ad85b1cbfb5c581a9148fdea2fdc169c39c5d80
necoder27 <laetitiaelbadry@gmail.com>
2024-12-22 02:22:21 +0100

    fix: fixed navbar showing/hiding with browser back button
    added conditional margin on top of router view for when navbar is shown

--- l-el/src/App.vue
+++ l-el/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {RouterView, useRoute} from 'vue-router'
   import Navbar from "@/components/Navbar.vue";
-  import {provide, ref} from "vue";
+import {provide, ref, watch} from "vue";
 
   const route = useRoute();
   const selectedPage = ref(route.path.split("/").pop() as string);
@@ -10,13 +10,18 @@ import {RouterView, useRoute} from 'vue-router'
     selectedPage.value = newPage;
   }
 
+  watch(
+    () => route.path,
+    (newPath) => { selectedPage.value = newPath.split("/").pop() as string; }
+  )
+
   provide("selectedPage", {selectedPage, changePage});
 </script>
 
 <template>
   <div class="flex flex-col h-screen overflow-x-hidden">
-    <Navbar v-if="selectedPage != 'home'" />
-    <RouterView />
+    <Navbar v-show="selectedPage != 'home' && selectedPage != ''" />
+    <RouterView :class="[(selectedPage != 'home' && selectedPage != '') ? 'mt-36' : 'mt-0']" />
   </div>
 </template>
 
