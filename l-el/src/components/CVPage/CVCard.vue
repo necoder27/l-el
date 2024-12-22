91cf762ee07837e8ad76ddbf3540e2d1a0f1cf77
necoder27 <laetitiaelbadry@gmail.com>
2024-12-22 02:21:58 +0100

    minor: changed shadow to hover effect

--- l-el/src/components/CVPage/CVCard.vue
+++ l-el/src/components/CVPage/CVCard.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="flex flex-col space-y-4 w-fit p-8 m-4 border border-gray-500 rounded-xl shadow-lg shadow-black">
+ <div class="flex flex-col space-y-4 w-fit p-8 m-4 border border-gray-500 rounded-xl hover:shadow-lg hover:shadow-black transition duration-300 ease-in-out">
    <span class="text-3xl"><strong>{{ title }}</strong></span>
    <slot></slot>
  </div>
