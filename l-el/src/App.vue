e36d4e5366ca0f468205bf6318ca4be2c92d3814
necoder27 <laetitiaelbadry@gmail.com>
2025-01-28 21:30:38 +0100

    minor: adjusted paddings and margins for medium screens, moved navbar above content

--- l-el/src/App.vue
+++ l-el/src/App.vue
@@ -20,8 +20,8 @@ import {provide, ref, watch} from "vue";
 
 <template>
   <div class="flex flex-col h-screen overflow-x-hidden">
-    <Navbar v-show="selectedPage != 'home' && selectedPage != ''" />
-    <RouterView :class="[(selectedPage != 'home' && selectedPage != '') ? 'mt-36' : 'mt-0']" />
+    <Navbar v-show="selectedPage != 'home' && selectedPage != ''" class="z-10" />
+    <RouterView :class="[(selectedPage != 'home' && selectedPage != '') ? 'mt-20 md:mt-36' : 'mt-0']" class="z-0" />
   </div>
 </template>
 
