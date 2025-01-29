e1f7d440cb0b6426f7e5285432293fdc0ec95c30
necoder27 <laetitiaelbadry@gmail.com>
2025-01-29 23:51:18 +0100

    feat: expanded project card and changed design

--- l-el/src/components/ProjectsPage/ProjectCard.vue
+++ l-el/src/components/ProjectsPage/ProjectCard.vue
@@ -3,19 +3,33 @@
 
   defineProps<{
     title: string;
+    tags: string[];
     description: string;
     image?: IImage;
+    link: string;
   }>();
 </script>
 
 <template>
-  <div class="flex flex-col w-fit border border-gray-500 p-8 rounded-xl hover:shadow-lg hover:shadow-[#171717] select-none transition duration-300 ease-in-out">
-    <img :src="image?.path" :alt="image?.alt" />
-    <span class="text-3xl">
-      <strong>{{ title }}</strong>
-    </span>
-    <p>{{ description }}</p>
-  </div>
+  <a :href="link" class="w-full xl:w-[30vw] mb-5 border border-gray-500 border-l-transparent border-t-transparent rounded-xl xl:hover:shadow-xl hover:shadow-black transition duration-300 ease-in-out">
+    <div class="flex flex-col justify-between w-full xl:w-[30vw] xl:h-full p-5">
+      <div class="flex flex-col space-y-2">
+        <img :src="image?.path" :alt="image?.alt" />
+        <span class="text-2xl lg:text-3xl underline decoration-gray-500">
+        <strong>{{ title }}</strong>
+      </span>
+        <div class="flex">
+          <a href="">
+            <img src="@/assets/imgs/socials_logos/github-mark/github-mark-white.png" class="h-5" alt="GitHub Logo">
+          </a>
+        </div>
+        <p class="text-justify lg:line-clamp-4 lg:text-ellipsis lg:overflow-hidden">{{ description }}</p>
+      </div>
+      <div class="flex flex-wrap">
+        <div v-for="tag in tags" class="border border-gray-500 border-l-transparent border-t-transparent text-gray-500 px-2 py-1 rounded-md w-fit mr-2 mt-2">{{ tag }}</div>
+      </div>
+    </div>
+  </a>
 </template>
 
 <style scoped>
--- l-el/src/views/ProjectsView.vue
+++ l-el/src/views/ProjectsView.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="flex flex-wrap mx-6 my-7">
-    <ProjectCard title="Test" description="hdjskvnejdkalf< fnjka lnfje fnja nj wjewihfj skfnfnejkfljfnej fjfj" />
+  <div class="flex flex-wrap w-[100%] px-6 my-7 justify-between mb-14">
+    <ProjectCard title="TestTestTestTestTest Test TestTest"
+                 :tags="['html', 'css', 'python']"
+                 description="hdjskvnejdkalf< fnjka lnfje fnja nj wjewihfj skfnfnejkfljfnej fjfjdhjskfh fhjkdshfjdskhfdjka fhhfjdksfhdjsfh fhdjksfhjdkhsfdjksfheiufhdj fhjdshfjdks cjkdsjc dkjcns cd cdkscndjskv cdkcvnsdksdk fdsc njskvjcndk cnjdksvn cd djcksv fvfdvfdvfd vndkj"
+                 link=""
+    />
+    <ProjectCard title="Project"
+                 :tags="['java']"
+                 description="hdjskvnejdkalf< fnjka lnfje fnja nj"
+                 link=""
+    />
+    <ProjectCard title="Yes"
+                 :tags="['python', 'sql']"
+                 description="hdjskvnejdkalf< fnjka lnfje fnja nj wjewihfj skfnfnejkfljfnej fjfjdhjskfh fhjkdshfjdskhfdjka"
+                 link=""
+    />
+    <ProjectCard title="Nope"
+                 :tags="['java', 'vue.js', 'tailwindcss', 'typescript', 'css', 'python', 'css', 'html']"
+                 description="hdjskvnejdkalf< fnjka lnfje fnja nj wjewihfj skfnfnejkfljfnej fjfjdhjskfh fhjkdshfjdskhfdjka fhhfjdksfhdjsfh"
+                 link=""
+    />
   </div>
 </template>
 
