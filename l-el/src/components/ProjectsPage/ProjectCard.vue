7e08b28dfe346ed5fe8eed531988ce3d71651a4c
necoder27 <laetitiaelbadry@gmail.com>
2025-02-02 18:40:05 +0100

    feat: reworked project card

--- l-el/src/components/ProjectsPage/ProjectCard.vue
+++ l-el/src/components/ProjectsPage/ProjectCard.vue
@@ -1,35 +1,48 @@
 <script setup lang="ts">
   import type {IImage} from "@/interfaces/image.interface.ts";
+  import {ref} from "vue";
 
   defineProps<{
     title: string;
     tags: string[];
     description: string;
     image?: IImage;
-    link: string;
+    links?: {
+      image: IImage;
+      link: string;
+    }[];
   }>();
+
+  const expanded = ref('');
+
+  function expandProjectDescription(projectName: string): void {
+    if (expanded.value === projectName) expanded.value = '';
+    else expanded.value = projectName;
+  }
 </script>
 
 <template>
-  <a :href="link" class="w-full xl:w-[30vw] mb-5 border border-gray-500 border-l-transparent border-t-transparent rounded-xl xl:hover:shadow-xl hover:shadow-black transition duration-300 ease-in-out">
-    <div class="flex flex-col justify-between w-full xl:w-[30vw] xl:h-full p-5">
-      <div class="flex flex-col space-y-2">
-        <img :src="image?.path" :alt="image?.alt" />
-        <span class="text-2xl lg:text-3xl underline decoration-gray-500">
+  <div class="flex flex-col justify-between w-full lg:w-[45vw] xl:w-[30vw] p-5 mb-5 border border-gray-500 border-l-transparent border-t-transparent rounded-xl xl:hover:shadow-xl hover:shadow-black transition duration-300 ease-in-out"
+       @click="expandProjectDescription(title)"
+  >
+    <div class="flex flex-col space-y-2">
+      <img :src="image?.path" :alt="image?.alt" />
+      <span class="text-2xl lg:text-3xl underline decoration-gray-500">
         <strong>{{ title }}</strong>
       </span>
-        <div class="flex">
-          <a href="">
-            <img src="@/assets/imgs/socials_logos/github-mark/github-mark-white.png" class="h-5" alt="GitHub Logo">
-          </a>
-        </div>
-        <p class="text-justify lg:line-clamp-4 lg:text-ellipsis lg:overflow-hidden">{{ description }}</p>
+      <div class="flex">
+        <a v-for="l in links" :href="l.link">
+          <img :src="l.image.path" class="h-5" :alt="l.image.alt">
+        </a>
       </div>
-      <div class="flex flex-wrap">
-        <div v-for="tag in tags" class="border border-gray-500 border-l-transparent border-t-transparent text-gray-500 px-2 py-1 rounded-md w-fit mr-2 mt-2">{{ tag }}</div>
-      </div>
+      <p :class="[expanded === title ? 'line-clamp-none' : 'line-clamp-6 lg:line-clamp-4', 'text-justify lg:text-ellipsis lg:overflow-hidden cursor-pointer']">
+        {{ description }}
+      </p>
     </div>
-  </a>
+    <div class="flex flex-wrap">
+      <div v-for="tag in tags" class="border border-gray-500 border-l-transparent border-t-transparent text-gray-500 px-2 py-1 rounded-md w-fit mr-2 mt-2 select-none">{{ tag }}</div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
