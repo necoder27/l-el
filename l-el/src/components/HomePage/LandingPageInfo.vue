fcd84a2a358d9df9f4782e6e9e121e9b221c3615
necoder27 <laetitiaelbadry@gmail.com>
2025-02-02 22:17:59 +0100

    minor: added linkedin link to landing page

--- l-el/src/components/HomePage/LandingPageInfo.vue
+++ l-el/src/components/HomePage/LandingPageInfo.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="flex flex-col h-[10vh] text-sm xs:text-sm md:text-xl m-5 md:m-10 font-light lg:font-thin">
     <span>Software Developer, CS Student &commat; TU Vienna</span>
-    <div class="flex justify-center w-full mt-1">
+    <div class="flex justify-center w-full mt-1 space-x-3">
       <a href="https://github.com/necoder27">
-        <img src="@/assets/imgs/socials_logos/github-mark/github-mark-white.png"
+        <img src="@/assets/imgs/logos/github-mark/github-mark-white.png"
              class="h-5 md:h-7"
              alt="github logo">
+      </a>
+      <a href="https://www.linkedin.com/in/laetitia-e-9b7a6b228/">
+        <img src="@/assets/imgs/logos/LinkedIn-Logos/LI-In-Bug.png"
+             class="h-5 md:h-7"
+             alt="linkedin logo"
+        >
       </a>
     </div>
   </div>
