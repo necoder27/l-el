9b44c692a160298d8f266ebd65a2ec37f65dd79f
necoder27 <laetitiaelbadry@gmail.com>
2025-03-06 14:04:31 +0100

    feat: extended the about page description
    ran build

--- src/components/AboutPage/AboutPageDescription.vue
+++ src/components/AboutPage/AboutPageDescription.vue
@@ -8,7 +8,8 @@
       hi! my name is <span class="text-nowrap underline decoration-gray-500"><strong>Laetitia El Badry</strong></span> and i'm a computer science student at the technical university of vienna.
       prior to my pursuit of a bachelor's degree i graduated from the bilingual (german, english) class of the
       computer science department at htl spengergasse.<sup><a href="#footnoteHTL" class="text-gray-500">[1]</a></sup>
-      i enjoy coding all kinds of things and like exploring and learning.
+      i enjoy coding all kinds of things and like exploring and learning. however, my special interest lies in ai and i hope to
+      be able to contribute to the amazing things that can be done with it.
     </p>
     <p id="footnoteHTL" class="pt-4 text-sm lg:text-md select-none">
       1. htl stands for "h&ouml;here technische lehranstalt" ("higher technical education institute") in german. it refers
