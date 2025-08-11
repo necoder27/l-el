db36345db4b028f2f3b80e349fe16656870c1ea6
necoder27 <laetitiaelbadry@gmail.com>
2025-08-12 00:01:24 +0300

    feat: replaced cs student at tu vienna status with stb ai student at jku

--- src/components/AboutPage/AboutPageDescription.vue
+++ src/components/AboutPage/AboutPageDescription.vue
@@ -5,11 +5,12 @@
 <template>
   <div>
     <p class="text-xl lg:text-3xl lg:tracking-wider">
-      hello there! my name is <span class="text-nowrap underline decoration-gray-500"><strong>Laetitia El Badry</strong></span> and i'm a computer science student at the technical university of vienna.
+      hey there! my name is <span class="text-nowrap underline decoration-gray-500"><strong>Laetitia El Badry</strong></span> and i'm a soon-to-be artificial intelligence student at johannes kepler university linz.
       prior to my pursuit of a bachelor's degree i graduated from the bilingual (german, english) class of the
       computer science department at htl spengergasse.<sup><a href="#footnoteHTL" class="text-gray-500">[1]</a></sup>
-      i enjoy coding all kinds of things and like exploring and learning. however, my special interest lies in ai and i hope to
-      contribute to the amazing things that can be done with it.
+      after finishing school i started studying computer science at the technical university of vienna but after completing a few courses i
+      made the decision to specialise in ai by switching university.
+      i enjoy coding all kinds of things and like exploring and learning. however, my special interests lie in ai and frontend development.
     </p>
     <p id="footnoteHTL" class="pt-4 text-sm lg:text-md select-none">
       1. htl stands for "h&ouml;here technische lehranstalt" ("higher technical education institute") in german. it refers
